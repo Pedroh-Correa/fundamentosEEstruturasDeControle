6e12287ade1d11a64d746ebe982c764e4670a541
Pedroh-Correa <phrnunescorrea@gmail.com>
2025-08-17 12:10:11 -0300

    First change

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,45 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("********************************");
+        System.out.println("Bem vindo a calculadora de media");
+
+        System.out.println("Digite a primeira nota:");
+        int nota1 =  scanner.nextInt();
+
+        System.out.println("Digite a primeira nota:");
+        int nota2 =  scanner.nextInt();
+
+        System.out.println("Digite a primeira nota:");
+        int nota3 =  scanner.nextInt();
+
+        double media = (nota1+nota2+nota3) / 3;
+
+        if(media>=7){
+            System.out.println("Aprovado");
+        }else{
+            if(media<7 && media >= 5){
+                System.out.println("Recuperacao");
+            }else{
+                System.out.println("Reprovado");
+            }
         }
+
+        System.out.println("Bem vindo a tabuada digital");
+        System.out.println("Digite o numero que gostaria de saber a tabuada do 1 ao 10");
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            int resultado = num * i;
+            System.out.println(resultado);
+        }
+
+
+        scanner.close();
     }
 }
